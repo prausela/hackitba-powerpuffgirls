3cbbf223926ef148e0690b66c080e72f70ab41ee
odevincenti <odevincenti@outlook.com>
2022-04-02 23:13:09 -0300

    Agrego Login

--- powerpuff/src/App.js
+++ powerpuff/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Container, Card, Button } from 'react-bootstrap';
 import Navigation from './Navigation';
+import UserAndPassword from './Forms';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const incrementCounter = () => {
+    return setCount(count+1);
+  }
   return (
     <>
       <header>
         <Navigation p={"hola"}/>
       </header>
       <main>
-        hola
+        {/*count*/}
+        {/*<Button onClick={incrementCounter}/>*/}
+        <Container className="w-50 pt-5">
+          <Card >
+            <Card.Header>Welcome</Card.Header>
+            <Card.Body>
+              <UserAndPassword/>
+              <Button variant="primary">Log In</Button>
+            </Card.Body>
+          </Card>
+        </Container>
       </main>
       <footer>
 
